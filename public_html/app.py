019d6e5069f292984a52187e1c50a1db55c7f87c
Jan Tarczoń <jan.tarczon@student.uj.edu.pl>
2026-01-17 16:52:54 +0100

    Dodano jedną wartość

--- public_html/app.py
+++ public_html/app.py
@@ -2,11 +2,10 @@ from flask import Flask, send_from_directory
 
 app = Flask(__name__)
 
-@app.route('/<path:path>')
-def send_report(path):
-    # Using request args for path will expose you to directory traversal attacks
-    return send_from_directory('', path)
-
 @app.route('/')
 def serve_home():
     return send_from_directory('', "index.html")
+
+@app.route('/exchange-market.htm')
+def serve_market():
+    return send_from_directory('', "exchange-market.htm")
