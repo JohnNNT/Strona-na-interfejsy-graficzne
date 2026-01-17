3e7687e525175bf868afefd073194a5d6d903f4d
Jan Tarczoń <jan.tarczon@student.uj.edu.pl>
2026-01-17 16:44:36 +0100

    Poprawka1

--- public_html/app.py
+++ public_html/app.py
@@ -5,4 +5,4 @@ app = Flask(__name__)
 @app.route('/<path:path>')
 def send_report(path):
     # Using request args for path will expose you to directory traversal attacks
-    return send_from_directory('/', path)
+    return send_from_directory('', path)
